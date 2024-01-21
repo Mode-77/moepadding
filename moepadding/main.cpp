d6e941f42479c75695dbff948704165b8313748d
Jared Thomas <sci.imagino@gmail.com>
2024-01-21 12:34:37 -0500

    Counts non-whitespace characters

--- moepadding/main.cpp
+++ moepadding/main.cpp
@@ -1,7 +1,38 @@
+#include <cassert>
+#include <cstdio>
 #include <iostream>
 
-int main()
+/*
+    Count non-whitespace characters
+*/
+int characterCount(std::FILE *file)
 {
-    std::cout << "hello, world\n";
+    assert(file);
+    int result = 0;
+    int character = std::fgetc(file);
+    while (character != EOF) {
+        if (!(character == ' ' || character == '\t' || character == '\n')) {
+            ++result;
+        }
+        character = std::fgetc(file);
+    }
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1) {
+        std::cout << "Must tell me the filename\n";
+        return EXIT_FAILURE;
+    }
+    std::string filename(argv[1]);
+    std::FILE *file = nullptr;
+    // Open file for reading.
+    fopen_s(&file, filename.c_str(), "r");
+    if (file == nullptr) {
+        std::cout << "File couldn't be opened\n";
+        return EXIT_FAILURE;
+    }
+    std::cout << "Character count: " << characterCount(file) << "\n";
     return 0;
 }
